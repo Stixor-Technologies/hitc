2de5d3ec8359859b8c42641384c794bb7f4d53cd
fawadmehmood1 <fawad.mehmood@stixor.com>
2024-07-09 17:38:04 +0500

    added container class

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,9 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  corePlugins: {
+    container: false,
+  },
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -19,9 +22,48 @@ const config: Config = {
         blue: "#588FFF",
         ferozi: "#28C2CB",
         pink: "#BF4CF4",
+        "light-black": "#1D1E1C",
+        "dark-gray": "#474747",
+      },
+      fontFamily: {
+        poppins: ["var(--font-poppins)"],
+      },
+      screens: {
+        xs: "440px",
       },
     },
   },
-  plugins: [],
+  plugins: [
+    function ({ addComponents }: any) {
+      addComponents({
+        ".container": {
+          width: "100%",
+          paddingLeft: "1rem",
+          paddingRight: "1rem",
+          marginLeft: "auto",
+          marginRight: "auto",
+          "@screen sm": {
+            maxWidth: "100%",
+            paddingLeft: "2rem",
+            paddingRight: "2rem",
+          },
+          "@screen md": {
+            maxWidth: "100%",
+          },
+          "@screen lg": {
+            maxWidth: "100%",
+            paddingLeft: "2.625rem",
+            paddingRight: "2.625rem",
+          },
+          "@screen xl": {
+            maxWidth: "100%",
+          },
+          "@screen 2xl": {
+            maxWidth: "1536px",
+          },
+        },
+      });
+    },
+  ],
 };
 export default config;
