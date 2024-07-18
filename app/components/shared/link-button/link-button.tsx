1e6332e569132cd841b4a64f1eb6d9cc8bf3ac87
fawadmehmood1 <fawad.mehmood@stixor.com>
2024-07-18 16:19:26 +0500

    fixed build issue

--- app/components/shared/link-button/link-button.tsx
+++ app/components/shared/link-button/link-button.tsx
@@ -35,6 +35,8 @@ const LinkButton: FC<ActionProps> = ({ variant, styles, text, ...props }) => {
   const allClassNames = `rounded-[0.625rem]  bg-gradient-to-b from-ferozi to-pink items-center justify-center gap-3 overflow-visible after:absolute after:-bottom-[3%] after:left-1/2 after:-z-10 after:h-[30%] after:w-[95%] after:-translate-x-[50%] after:rounded-full after:bg-gradient-to-b after:opacity-0 after:blur-lg after:duration-300 after:ease-in-out after:content-[''] hover:after:opacity-100 disabled:opacity-70 ${styles} ${getSizeClass()}`;
 
   if (props.as === "link") {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+
     const { as, ...rest } = props;
     return (
       <Link className={`${allClassNames} inline-flex`} {...rest}>
@@ -42,6 +44,8 @@ const LinkButton: FC<ActionProps> = ({ variant, styles, text, ...props }) => {
       </Link>
     );
   }
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
 
   const { as, loading, ...rest } = props;
   return (
