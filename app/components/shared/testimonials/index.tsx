f3cfef36c4c2020741e20c0738790093e584ea68
fawadmehmood1 <fawad.mehmood@stixor.com>
2024-07-18 11:59:24 +0500

    updated testimonial template

--- app/components/shared/testimonials/index.tsx
+++ app/components/shared/testimonials/index.tsx
@@ -17,55 +17,49 @@ const Testimonials: FC<TestimonialsProps> = ({ testimonials }) => {
       <div className="container">
         <h2 className="text-3xl lg:text-5xl">Testimonials</h2>
       </div>
-      <div className="container mt-7 px-0 lg:mt-10">
-        <div className="relative mt-3 h-[12rem] w-full cursor-pointer overflow-visible md:mt-10 lg:h-[16rem] ">
-          <div className="absolute left-0 list-none overflow-visible !px-4 sm:!px-8 lg:!px-[2.625rem]">
-            <Swiper
-              slidesPerView={"auto"}
-              freeMode={true}
-              modules={[FreeMode, Pagination]}
-              spaceBetween={40}
-              pagination={{
-                clickable: true,
-              }}
-              className="!overflow-visible "
-            >
-              {testimonials?.map((testimony) => {
-                return (
-                  <SwiperSlide
-                    key={testimony?.id}
-                    className={`relative w-full max-w-[26.375rem]`}
-                  >
-                    <ul className="relative flex flex-1">
-                      <li className="flex-1 rounded-2xl bg-smoke-black px-[2rem] py-[1.563rem] text-light-silver">
-                        <div className="flex items-center gap-5">
-                          <div className="relative size-11 overflow-hidden rounded-full">
-                            <Image
-                              src={testimony?.imageUrl}
-                              fill
-                              alt={`testimony-${testimony?.name}`}
-                              className="absolute object-cover"
-                            />
-                          </div>
-                          <div className="text-stixor-secondary text-left">
-                            <h4 className="text-white">{testimony?.name}</h4>
-                            <h5 className="mt-1 text-xs">
-                              {testimony?.designation}
-                            </h5>
-                          </div>
-                        </div>
+      <div className="container mt-7 lg:mt-10">
+        <ul className="mb-10 cursor-pointer overflow-visible">
+          <Swiper
+            slidesPerView={"auto"}
+            freeMode={true}
+            modules={[FreeMode, Pagination]}
+            spaceBetween={40}
+            pagination={{
+              clickable: true,
+            }}
+            className="!overflow-visible "
+          >
+            {testimonials?.map((testimony) => {
+              return (
+                <SwiperSlide
+                  key={testimony?.id}
+                  className={`w-full max-w-[26.375rem]`}
+                >
+                  <li className="flex-1 rounded-2xl bg-smoke-black px-[2rem] py-[1.563rem] text-light-silver">
+                    <div className="flex items-center gap-5">
+                      <div className="relative size-11 overflow-hidden rounded-full">
+                        <Image
+                          src={testimony?.imageUrl}
+                          fill
+                          alt={`testimony-${testimony?.name}`}
+                          className="absolute object-cover"
+                        />
+                      </div>
+                      <div className="text-stixor-secondary text-left">
+                        <h4 className="text-white">{testimony?.name}</h4>
+                        <h5 className="mt-1 text-xs">
+                          {testimony?.designation}
+                        </h5>
+                      </div>
+                    </div>
 
-                        <p className="mt-7 lg:text-lg">
-                          {testimony?.testimony}
-                        </p>
-                      </li>
-                    </ul>
-                  </SwiperSlide>
-                );
-              })}
-            </Swiper>
-          </div>
-        </div>
+                    <p className="mt-7 lg:text-lg">{testimony?.testimony}</p>
+                  </li>
+                </SwiperSlide>
+              );
+            })}
+          </Swiper>
+        </ul>
       </div>
     </section>
   );
