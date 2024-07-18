588f2144afe70989cfef2ded245f9eee0263fda5
fawadmehmood1 <fawad.mehmood@stixor.com>
2024-07-18 11:49:51 +0500

    updated map component

--- app/components/shared/map/map-component.tsx
+++ app/components/shared/map/map-component.tsx
@@ -1,14 +1,10 @@
 "use client";
-import React, { FC } from "react";
+import React from "react";
 import { GoogleMap, Marker, useJsApiLoader } from "@react-google-maps/api";
-// import Spinner from "../spinner/spinner";
+import Spinner from "../spinner/spinner";
 import { MAP_KEY } from "@/app/utils/constant";
 
-interface IProps {
-  fromContact?: boolean;
-}
-
-const MapComponent: FC<IProps> = ({ fromContact }) => {
+const MapComponent = () => {
   const { isLoaded } = useJsApiLoader({
     id: "google-map-script",
     googleMapsApiKey: MAP_KEY!,
@@ -33,7 +29,7 @@ const MapComponent: FC<IProps> = ({ fromContact }) => {
 
   return (
     <div
-      className={`sm:pb-1/2 relative h-[350px] w-full md:order-1 md:h-auto md:flex-1 `}
+      className={`sm:pb-1/2 relative h-[21.875rem] w-full md:order-1 md:h-auto md:flex-1 `}
     >
       {isLoaded ? (
         <GoogleMap
@@ -43,35 +39,19 @@ const MapComponent: FC<IProps> = ({ fromContact }) => {
           options={mapOptions}
           mapContainerClassName="absolute top-0 left-0 h-full w-full"
         >
-          <Marker position={location} />
+          <Marker
+            position={location}
+            icon={{
+              url: "/assets/location-marker.svg",
+            }}
+          />
         </GoogleMap>
       ) : (
         <div className="absolute left-0 top-0 flex h-full w-full items-center">
-          {/* <Spinner color="text-indigo-blue" /> */}
-          <p>Loading</p>
+          <Spinner color="text-dark-gray" />
         </div>
       )}
     </div>
-
-    // <div
-    //   className={`sm:pb-1/2 relative w-full flex-1 items-center ${fromContact ? "h-[22rem] sm:h-[20.688rem]" : "h-[22rem] sm:h-[34.563rem]"} `}
-    // >
-    //   {isLoaded ? (
-    //     <GoogleMap
-    //       id="google-map"
-    //       zoom={12}
-    //       center={location}
-    //       options={mapOptions}
-    //       mapContainerClassName="absolute top-0 left-0 h-full w-full"
-    //     >
-    //       <Marker position={location} />
-    //     </GoogleMap>
-    //   ) : (
-    //     <div className="absolute left-0 top-0 flex h-full w-full items-center">
-    //       Loading
-    //     </div>
-    //   )}
-    // </div>
   );
 };
 
